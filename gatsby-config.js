ceb671e06ca1ee37aa63231ecef5ac5921a33f60
Darby Clement <darbyclement@berkeley.edu>
2022-10-12 13:22:12 -0700

    ari

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,14 +2,5 @@ module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
     title: "ari",
-  },
-  plugins: [
-    "gatsby-plugin-gatsby-cloud",
-    {
-      resolve: "gatsby-plugin-google-analytics",
-      options: {
-        trackingId: "",
-      },
-    },
-  ],
+  }
 };
